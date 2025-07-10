5eaa68555c4f8c3449db6dfe203a953a6342f0e7
OLUWASEYI AYOKUNLE <oluwaseyiayokunle@gmail.com>
2025-07-10 16:29:20 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/app/editor/[id]/page.tsx
+++ src/app/editor/[id]/page.tsx
@@ -120,7 +120,7 @@ export default function EditorPage() {
       PasteHandler,
       ProgressBarBlock,
       FunctionPlot,
-      Calculator,
+      Calculator
     ],
     editorProps: {
       attributes: {
